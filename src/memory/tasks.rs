1b54799ab7bdee7b8d3ed9e896ee8885248b4a91
myFavShrimp <42263661+myFavShrimp@users.noreply.github.com>
2025-03-23 18:35:02 +0100

    Changed ordering back

--- src/memory/tasks.rs
+++ src/memory/tasks.rs
@@ -24,8 +24,7 @@ impl PartialOrd for Task {
                 let self_depends_on_other = self.dependencies.contains(&other.name);
 
                 match (other_depends_on_self, self_depends_on_other) {
-                    (false, false) => std::cmp::Ordering::Equal,
-                    (true, true) => return None,
+                    (true, true) | (false, false) => std::cmp::Ordering::Equal,
                     (true, false) => std::cmp::Ordering::Less,
                     (false, true) => std::cmp::Ordering::Greater,
                 }
