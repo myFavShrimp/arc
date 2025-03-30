2cee81cfa40e61cf5ab7335bee0316c2a0cb9654
myFavShrimp <42263661+myFavShrimp@users.noreply.github.com>
2025-03-30 19:29:03 +0200

    Completed logging implementation

--- src/engine/modules/log.rs
+++ src/engine/modules/log.rs
@@ -6,14 +6,17 @@ use super::MountToGlobals;
 pub struct Log;
 
 impl Log {
-    fn info(value: mlua::Value) {
-        let message = match value.to_string() {
-            Ok(utf8_str) => utf8_str,
-            Err(_) => {
-                format!("[binary data] {:X?}", value)
-            }
-        };
+    fn debug(message: &str) {
+        println!(
+            "{:.3} {}{}: {}",
+            jiff::Timestamp::now(),
+            "DEBUG".green(),
+            "".clear(),
+            message,
+        );
+    }
 
+    fn info(message: &str) {
         println!(
             "{:.3} {}{}: {}",
             jiff::Timestamp::now(),
@@ -22,12 +25,56 @@ impl Log {
             message,
         );
     }
+
+    fn warn(message: &str) {
+        println!(
+            "{:.3} {}{}: {}",
+            jiff::Timestamp::now(),
+            "WARN".yellow(),
+            "".clear(),
+            message,
+        );
+    }
+
+    fn error(message: &str) {
+        println!(
+            "{:.3} {}{}: {}",
+            jiff::Timestamp::now(),
+            "ERROR".red(),
+            "".clear(),
+            message,
+        );
+    }
+}
+
+fn lua_value_to_string(value: mlua::Value) -> String {
+    match value.to_string() {
+        Ok(utf8_str) => utf8_str,
+        Err(_) => {
+            format!("[binary data] {:X?}", value)
+        }
+    }
 }
 
 impl UserData for Log {
     fn add_methods<M: mlua::UserDataMethods<Self>>(methods: &mut M) {
+        methods.add_function("debug", |_, value: mlua::Value| {
+            Log::debug(&lua_value_to_string(value));
+
+            Ok(())
+        });
         methods.add_function("info", |_, value: mlua::Value| {
-            Log::info(value);
+            Log::info(&lua_value_to_string(value));
+
+            Ok(())
+        });
+        methods.add_function("warn", |_, value: mlua::Value| {
+            Log::warn(&lua_value_to_string(value));
+
+            Ok(())
+        });
+        methods.add_function("error", |_, value: mlua::Value| {
+            Log::error(&lua_value_to_string(value));
 
             Ok(())
         });
@@ -41,7 +88,7 @@ impl MountToGlobals for Log {
         globals.set(
             "print",
             lua.create_function(|_, value: mlua::Value| {
-                Log::info(value);
+                Log::info(&lua_value_to_string(value));
 
                 Ok(())
             })?,
